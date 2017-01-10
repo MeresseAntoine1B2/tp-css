96616a34cfa9aafd56073560a5e7704c7013e6ee
Antoine <antoine@AntoinePC.home>
2017-01-10 23:34:10 +0100

    slide jquery

--- js/voirplus.js
+++ js/voirplus.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
   
-	
+	$(".enSavoirPlus").prev().slideToggle();
 	$(".enSavoirPlus").click(function()
 	{
-		
+		$(this).prev().slideToggle();
 		if ($(this).html() == 'En savoir <span class="plus">+</span>')
 		{
-			$(this).prev().css("max-height", 1000);
 			$(this).html('Voir <span class="plus">-</span>');
 		}
 		else
 		{
-			$(this).prev().css("max-height",$(this).prev().height());
-			$(this).prev().css("max-height", "0");
 			$(this).html('En savoir <span class="plus">+</span>');
 		}
 	});
