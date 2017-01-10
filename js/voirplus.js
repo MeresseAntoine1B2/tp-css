99876c526bde5dcdb445f2145ee18f75a8589ec5
Antoine <antoine@AntoinePC.home>
2017-01-10 19:54:09 +0100

    Ajout JS fonctionnel et amélioration style

--- js/voirplus.js
+++ js/voirplus.js
@@ -1,4 +1,22 @@
-function voirplus()
-{
-    
-}
+$(document).ready(function() {
+  
+	
+	$(".enSavoirPlus").click(function()
+	{
+		
+		if ($(this).html() == 'En savoir <span class="plus">+</span>')
+		{
+			$(this).prev().css("max-height", 1000);
+			$(this).html('Voir <span class="plus">-</span>');
+		}
+		else
+		{
+			$(this).prev().css("max-height",$(this).prev().height());
+			$(this).prev().css("max-height", "0");
+			$(this).html('En savoir <span class="plus">+</span>');
+		}
+	});
+  
+  
+});
+
